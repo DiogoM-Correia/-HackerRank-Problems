9457f631c8cd9a1c0531343d5e22c8cd7aaf5ed1
DiogoM-Correia <55164816+DiogoM-Correia@users.noreply.github.com>
2023-03-09 18:04:57 +0000

    Create Counting Sort 1.cs

---
+++ Counting Sort 1.cs
@@ -0,0 +1,67 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'countingSort' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static List<int> countingSort(List<int> arr)
+    {
+        int size = arr.Count;
+        int max = arr.Max();
+
+        List<int> zerosList = new List<int> ();
+        
+        for (int i = 0; i < 100; i++)
+        {
+            zerosList.Add(0);
+        }
+        Console.WriteLine(zerosList.Count);
+        Console.WriteLine(size);
+        Console.WriteLine(max);
+        
+        for (int i = 0; i < size; i++)
+        {
+            zerosList[arr[i]] ++;
+        }
+        
+        return zerosList;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        List<int> result = Result.countingSort(arr);
+
+        textWriter.WriteLine(String.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
