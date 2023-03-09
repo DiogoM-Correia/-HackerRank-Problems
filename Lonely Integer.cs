c96d8a54794e9ffa118cecd2e3b161041b1a33cf
DiogoM-Correia <55164816+DiogoM-Correia@users.noreply.github.com>
2023-03-09 18:03:10 +0000

    Create Lonely Integer.cs

---
+++ Lonely Integer.cs
@@ -0,0 +1,68 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'lonelyinteger' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int lonelyinteger(List<int> a)
+    {
+        int size = a.Count();
+        for(int position = 0; position < size; position ++)
+        {
+            List<int> subList = new List<int> (a);
+            subList.RemoveAt(position);
+            int found = 0;
+            for(int i = 0; i < subList.Count; i++)
+            {
+                if (a.ElementAt(position) == subList.ElementAt(i))
+                {
+                    found ++;
+                }
+            }
+            
+            if (found == 0)
+            {
+                return a.ElementAt(position);
+            }
+        }
+        return -1;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        int result = Result.lonelyinteger(a);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
